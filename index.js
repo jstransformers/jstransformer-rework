576993976f9e20bf2cd141ec7368a60e4921be56
tunnckoCore <mameto_100@mail.bg>
2015-04-22 11:43:34 +0300

    add support for `options.plugins`

--- index.js
+++ index.js
@@ -13,15 +13,28 @@ exports.name = 'rework';
 exports.inputFormats = ['css', 'rework'];
 exports.outputFormat = 'css';
 
-exports.render = function _render(str, opts, plugins) {
-  plugins = plugins || [];
-  plugins = Array.isArray(plugins) ? plugins : [plugins];
+exports.render = function _render(str, options) {
+  options = typeof options === 'object' ? options : {};
+  options.plugins = arrayify(options.plugins);
 
-  var process = rework(str, opts);
+  var process = rework(str, options);
 
-  plugins.forEach(function(plugin) {
+  options.plugins.forEach(function(plugin) {
     process = process.use(plugin);
   });
 
-  return process.toString(opts);
+  return process.toString(options);
 };
+
+/**
+ * arrayify value
+ *
+ * @param  {*} `val`
+ * @return {Array}
+ * @api private
+ */
+function arrayify(val) {
+  return !Array.isArray(val)
+    ? [val]
+    : val;
+}
